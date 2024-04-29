c60f0723511b931ead08b9fc516290a61bc0a730
Abdul Rehamn <92205472+likeam@users.noreply.github.com>
2024-04-29 15:25:02 +0500

    Finshed

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,33 @@
+import { useState } from "react"
+import items from './data'
+import Categories from "./Categories";
+import Menu from "./Menu";
+
 const App = () => {
+
+  const [menuItems, setMenuItems] = useState(items);
+  const [categories, setCategories] = useState([]);
+ 
+  const filterItems = (category) => {
+    if(category === 'all'){
+      setMenuItems(items);
+      return;
+    }
+    const newItems = items.filter((item) => item.category === category )
+    setMenuItems(newItems)
+  }
+
   return (
-    <div>App</div>
+    <main>
+      <section className="menu section ">
+        <div className="titie">
+          <h2>our menu</h2>
+          <div className="underline"></div>
+        </div>
+        <Categories filterItems={filterItems} />
+        <Menu items={menuItems}/>
+      </section>
+    </main>
   )
 }
 export default App
--- src/Categories.jsx
+++ src/Categories.jsx
@@ -1,6 +1,13 @@
-const Categories = () => {
+import items from './data'
+
+const Categories = ({filterItems}) => {
   return (
-    <div>Categories</div>
+    <div className='btn-container'>
+        <button className="filter-btn" onClick={() => filterItems('all')}>All</button>
+        <button className="filter-btn" onClick={() => filterItems('breakfast')}>Breakfast</button>
+        <button className="filter-btn" onClick={() => filterItems('lunch')}>Lunch</button>
+        <button className="filter-btn" onClick={() => filterItems('shakes')}>Shaks</button>
+    </div>
   )
 }
 export default Categories
